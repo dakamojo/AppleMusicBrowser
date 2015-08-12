004ca077772682e9689d5884d36509eb4739d55f
DDavis <ddavis@PLNA003130-MBP.local>
2015-08-12 10:59:24 -0500

    Updated AppleMusicClient

---
+++ AppleMusicBrowser/Models/AppleMusicClient.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace AppleMusicBrowser
+{
+	public static class AppleMusicClient
+	{
+		public static async Task<ObservableCollection<Album>> GetTopAlbumFeed(int limit) {
+//			Url url = new Url ("https://itunes.apple.com/us/rss/topalbums/json");
+//			var data = await url.GetJsonAsync ();
+
+			Assembly assembly = typeof(AppleMusicClient).GetTypeInfo().Assembly;
+			Stream stream = assembly.GetManifestResourceStream("AppleMusicBrowser.Models.SampleAlbumData.json");
+			StreamReader reader = new StreamReader (stream);
+
+			ObservableCollection<Album> albumList = await JsonConvert.DeserializeObjectAsync<ObservableCollection<Album>> (reader.ReadToEnd ());
+
+			return albumList;
+		}
+
+	}
+}
+
